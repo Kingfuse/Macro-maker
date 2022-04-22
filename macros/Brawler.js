9150a77bcecf04c5ec7a7c129f370b123f5193a7
Kingfuse <66550030+Kingfuse@users.noreply.github.com>
2022-04-22 13:29:50 +0200

    Add files via upload

--- macros/Brawler.js
+++ macros/Brawler.js
@@ -1,19 +1,30 @@
 module.exports = {
-    enabled: true,
-    toggleRepeaterKey: "Numpad1",
+    enabled: false,
+    toggleRepeaterKey: "\\",
     skills: {
-        // Hailstorm
-        "1": {
-            enabled: true,
-            key: "&",
-			onPress: [
-				{ action: "keyRepeat", key: "Numpad2", duration: 400, interval: 100, delay: 300, fixedDelay: true }
-            ],
-            repeater: false,
-        },
-    }
-    ,
-
+        "1": false, // Punch
+		"2": false, // Counter
+		"3": false, // Divine Wrath
+		"4": false, // Ground Pound
+		"5": false, // Bullrush
+		"6": false, // Haymaker
+		"7": false, // Roundhouse Kick
+		"8": false, // Piledriver
+		"9": false, // Jackhammer
+		"10": false, // Counterpunch
+		"12": false, // Retaliate
+		"13": false, // Provoke
+		"14": false, // Infuriate
+		"16": false, // Flip Kick
+		"18": false, // Growing Fury
+		"19": false, // Invigorating Rage
+		"21": false, // Mounting Rage
+		"40": false, // Quick Dash
+		"22": false, // Flying Kick
+		"24": false, // 2.54cm (One Inch) Punch
+		"26": false, // Brawling Roll (Ult)
+		"910": false, // Apex Urgency
+    },
     hotkeys: {
     }
 }
